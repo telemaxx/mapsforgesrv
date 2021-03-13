e15cf3980671651c1fab8826f3ee4f44b5eb7d9a
Thomas Th. <6745th@web.de>
2021-03-13 12:41:54 +0100

    created new project from github/mapsforgesrv_with_gradle
    starting buildsystem github 7.0 from sratch

--- mapsforgesrv/src/main/java/com/telemaxx/mapsforgesrv/MapsforgeSrv.java
+++ mapsforgesrv/src/main/java/com/telemaxx/mapsforgesrv/MapsforgeSrv.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright 2016 r_x
- * Copyright 2019, 2020 Thomas Theussing and Contributors
+ * Copyright 2019, 2020, 2021 Thomas Theussing and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
