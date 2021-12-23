1628a70d85160c2fb0c4c1c8f3bd856dd78df10b
nono303 <git@nono303.net>
2021-12-23 16:05:46 +0100

    improve gc log filename with ymdhms

--- mapsforgesrv/launch.sample.bat
+++ mapsforgesrv/launch.sample.bat
@@ -1,8 +1,10 @@
 REM https://docs.oracle.com/javase/8/docs/technotes/guides/2d/flags.html#win
 REM https://github.com/bourgesl/mapbench/blob/master/bin/env_marlin.sh
+SET ymdhis=%date:~-4,4%-%date:~-10,2%-%date:~-7,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
+SET ymdhis=%ymdhis: =0%
 java.exe ^
 	--patch-module java.desktop=libs\marlin-0.9.4.5-Unsafe-OpenJDK11.jar ^
-	-Xlog:gc:D:\mapsforgesrv\logs\gc.log:time,level,tags ^
+	-Xlog:gc:D:\mapsforgesrv\logs\gc.%ymdhis%.log:time,level,tags ^
 	-Xmx2G ^
 	-Xms2G ^
 	-Dsun.java2d.renderer.log=true ^
