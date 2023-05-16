859b9ab858f9c218bd9bbc00376628978cbb6d64
Thomas Th. <6745th@web.de>
2023-05-16 17:17:41 +0200

    fixed setting Java 8 Binaries.
    But did not run on newer Java due Jetty incompability with newer Javas

---
+++ mapsforgesrv/bin/jars_ready2use/start_mf.bat
@@ -0,0 +1,1 @@
+java -Xmx1G -Xms256M -Xmn256M -jar C:/Users/top/git/mapsforgesrv/mapsforgesrv/bin/jars_ready2use/mapsforgesrv-fatjar.jar -if all -p 8080 -m D:/BTSync/oruxmaps/mapfiles/Germany_V3_oam.map -t D:/BTSync/oruxmaps/mapstyles/ELV5/Elements.xml -s elmt-hiking -l de -r database -tms https://a.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png
