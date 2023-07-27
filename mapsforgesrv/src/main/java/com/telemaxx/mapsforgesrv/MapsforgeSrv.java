bae17ce943aae0e0b1086028a1adf9c9522e3f6e
Thomas Th. <6745th@web.de>
2023-07-27 13:04:58 +0200

    0.19.1

--- mapsforgesrv/src/main/java/com/telemaxx/mapsforgesrv/MapsforgeSrv.java
+++ mapsforgesrv/src/main/java/com/telemaxx/mapsforgesrv/MapsforgeSrv.java
@@ -44,6 +44,7 @@
  *         - only from loopback addresses
  *         - and only when accepted
  *         set maximum line length of "help" to 132 characters (JFritzle)
+ * 0.19.1: raise version to 0.19.1
  ******************************************************************************/
 
 package com.telemaxx.mapsforgesrv;
@@ -65,7 +66,7 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class MapsforgeSrv {
 	
-	private final static String VERSION = "0.19.0"; // starting with eg 0.13, the mapsforge version //$NON-NLS-1$
+	private final static String VERSION = "0.19.1"; // starting with eg 0.13, the mapsforge version //$NON-NLS-1$
 	
 	final static Logger logger = LoggerFactory.getLogger(MapsforgeSrv.class);
 
