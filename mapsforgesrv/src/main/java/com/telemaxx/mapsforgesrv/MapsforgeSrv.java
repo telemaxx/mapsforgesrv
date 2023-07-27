c5c6c11eef53549dd5cbed53ed5936f2a6026832
Thomas Th. <6745th@web.de>
2023-07-27 13:05:10 +0200

    0.19.1

--- mapsforgesrv/src/main/java/com/telemaxx/mapsforgesrv/MapsforgeSrv.java
+++ mapsforgesrv/src/main/java/com/telemaxx/mapsforgesrv/MapsforgeSrv.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright 2016 r_x
- * Copyright 2019, 2021, 2022 Thomas Theussing and Contributors
+ * Copyright 2019, 2021, 2022, 2023 Thomas Theussing and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
